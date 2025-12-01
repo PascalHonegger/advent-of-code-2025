42af20b407fc497a8fce655f9070943048127db9
Pascal Honegger <git@honegger.dev>
2025-12-01 06:31:35 +0100

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,57 @@
 fun main() {
+    data class Rotation(val direction: Char, val amount: Int)
+    class Dial {
+        private var orientation: Int = 50
+        var passedZeroes: Int = 0
+        var landedOnZeroes: Int = 0
+        operator fun plusAssign(rotation: Rotation) {
+            // Very ugly but it's too early for my brain to fix it
+            if (orientation == 0 && rotation.direction == 'L') passedZeroes--
+            if (rotation.direction == 'L') {
+                orientation -= rotation.amount
+            } else {
+                orientation += rotation.amount
+            }
+            while (orientation > 100) {
+                orientation -= 100
+                passedZeroes++
+            }
+            while (orientation < 0) {
+                orientation += 100
+                passedZeroes++
+            }
+            if (orientation == 0 || orientation == 100) {
+                passedZeroes++
+                orientation = 0
+            }
+            if (orientation == 0) {
+                landedOnZeroes++
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val rotations = input.map { Rotation(it.first(), it.drop(1).toInt()) }
+        val dial = Dial()
+        for (rotation in rotations) {
+            dial += rotation
+        }
+        return dial.landedOnZeroes
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val rotations = input.map { Rotation(it.first(), it.drop(1).toInt()) }
+        val dial = Dial()
+        for (rotation in rotations) {
+            dial += rotation
+        }
+        return dial.passedZeroes
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
